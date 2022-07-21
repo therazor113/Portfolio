34bf11b47d5ff5307e9bca32d7c6db5fee609a20
Razor <therazor113@gmail.com>
2022-07-21 19:29:15 -0400

    Created Introduction

---
+++ src/components/introduction/IntroView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Hey, I'm Zack 👋</h2>
+    <h2>Front End Devoloper</h2>
+  </div>
+</template>
+
+<style lang='scss' scoped>
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
