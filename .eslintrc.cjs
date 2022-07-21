d545fab11aa03fc083974f381cdfa0f7a7d24e95
Razor <therazor113@gmail.com>
2022-07-21 15:22:29 -0400

    lint fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,10 @@
 /* eslint-env node */
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended"
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'standard'
   ]
 }
