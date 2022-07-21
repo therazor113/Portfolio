c999b6ef91db98b6a3f22f9531f0524bdfd19942
Razor <therazor113@gmail.com>
2022-07-21 19:28:52 -0400

    Created Footer

---
+++ src/components/footer/FooterView.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer>
+    Powered by:
+    <a
+      href="https://vuejs.org/"
+    >
+      <img
+        alt="Vue logo"
+        src="@/assets/logo.svg"
+        width="25"
+        height="25"
+      >
+    </a>
+  </footer>
+</template>
+
+<style lang='scss'>
+  footer {
+    border-top: 1px solid black;
+    padding-top: 1rem;
+    display: flex;
+    justify-content: center;
+    img {
+      margin-left: 5px;
+    }
+  }
+</style>
