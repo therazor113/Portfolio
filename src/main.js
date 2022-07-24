a6b73f424d9a3b02d107273aa4ce44503e07d43e
Razor <therazor113@gmail.com>
2022-07-24 14:06:54 -0400

    Shortened app

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ScrollTo from '@/scrollTo/ScrollTo.vue'
 import './assets/base.css'
 
-const app = createApp(App)
-
-app.mount('#app')
+createApp(App).component('ScrollLink', ScrollTo).mount('#app')
