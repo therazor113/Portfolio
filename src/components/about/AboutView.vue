0c55e29c5652a3f9238354947b22634d4f08e931
Razor <therazor113@gmail.com>
2022-07-28 13:19:10 -0400

    Created About section

---
+++ src/components/about/AboutView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="aboutContainer">
+    <h1 class="title">
+      About Me
+    </h1>
+    <h1 class="header">
+      Introduction
+    </h1>
+    <h2>
+      I’m a self-taught web developer with experience in designing web applications from idea to product,
+      implementation of new technologies and design flows, and working with a variety of different languages and frameworks.
+      I strive to design the best user experience possible, while maintaining a clean and efficient coding environment.
+      My passion is to provide great design, reusability and innovation in all my projects.
+    </h2>
+    <br>
+    <h2>
+      Currently focused on front-end development, but also studying for full-stack development.
+    </h2>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .title {
+    color: #fff;
+    font-size: 40px;
+    text-align: center;
+  }
+  .aboutContainer {
+    margin-bottom: 2rem;
+    .header {
+      color: #fff;
+      text-decoration: underline;
+      margin-bottom: 1rem;
+    }
+  }
+</style>
