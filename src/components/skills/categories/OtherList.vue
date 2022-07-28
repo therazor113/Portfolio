a0e62ed791489777e01437e6897e20185fd3f743
Razor <therazor113@gmail.com>
2022-07-28 13:21:31 -0400

    Added classes to view

--- src/components/skills/categories/OtherList.vue
+++ src/components/skills/categories/OtherList.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="other">
-    <h2>Other</h2>
+    <h2 class="title">
+      Other
+    </h2>
     <div class="icons">
       <a href="https://vercel.com/">
         <img
@@ -11,6 +13,7 @@
           width="65"
           height="65"
         >
+        <h2>Vercel</h2>
       </a>
       <a href="https://vitejs.dev/">
         <img
@@ -20,6 +23,7 @@
           width="65"
           height="65"
         >
+        <h2>Vite</h2>
       </a>
       <a href="https://code.visualstudio.com/">
         <img
@@ -29,6 +33,7 @@
           width="65"
           height="65"
         >
+        <h2>VSCode</h2>
       </a>
       <a href="https://www.postgresql.org/">
         <img
@@ -38,38 +43,55 @@
           width="65"
           height="65"
         >
+        <h2>PostgreSQL</h2>
       </a>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-    .other {
-      display: flex;
-      flex-direction: column;
-      width: fit-content;
-      text-align: center;
+  .other {
+    display: flex;
+    flex-direction: column;
+    width: fit-content;
+    text-align: center;
+    .title {
+      margin-bottom: 10px;
+      text-decoration: underline;
+    }
+  }
+  .icons {
+    display: flex;
+    justify-content: center;
+    width: fit-content;
+    height: 80px;
+    img {
+      transition: transform 0.2s, filter 0.2s;
+      margin: 5px;
+      &:hover:not(.verc) {
+        filter: drop-shadow(5px 5px 5px black);
+        transform: scale(1.2);
+      }
+      &:hover.verc {
+        filter: drop-shadow(4px 4px 4px rgb(35, 35, 35));
+        transform: scale(1.2);
+      }
+    }
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+    h2 {
+      display: none;
+    }
+    &:hover {
       h2 {
-        margin-bottom: 10px;
-        text-decoration: underline;
+        position: absolute;
+        top: 115%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        display: block;
       }
     }
-    .icons {
-      display: flex;
-      justify-content: center;
-      width: 500px;
-      height: 80px;
-      img {
-        transition: transform 0.2s, filter 0.2s;
-        margin: 5px;
-        &:hover:not(.verc) {
-          filter: drop-shadow(5px 5px 5px black);
-          transform: scale(1.2);
-        }
-        &:hover.verc {
-          filter: drop-shadow(4px 4px 4px rgb(35, 35, 35));
-          transform: scale(1.2);
-        }
-      }
-    }
+  }
 </style>
