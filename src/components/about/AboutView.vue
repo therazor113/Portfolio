acbaf3166c4ec27e34519ba1d0bf8e540745df32
Razor <therazor113@gmail.com>
2022-07-31 15:34:15 -0400

    Edit to about info

--- src/components/about/AboutView.vue
+++ src/components/about/AboutView.vue
@@ -8,13 +8,14 @@
     </h1>
     <h2>
       I’m a self-taught web developer with experience in designing web applications from idea to product,
-      implementation of new technologies and design flows, and working with a variety of different languages and frameworks.
-      I strive to design the best user experience possible, while maintaining a clean and efficient coding environment.
-      My passion is to provide great design, reusability and innovation in all my projects.
+      working with a variety of different languages/frameworks,
+      along with implementation of new and ever-changing technologies. I strive to design the best user experience possible,
+      while maintaining clean and efficient coding environments.
+      My passion is to provide great design, reusability and innovation in all of my projects.
     </h2>
     <br>
     <h2>
-      Currently focused on front-end development, but also studying for full-stack development.
+      Currently focused on full-stack development.
     </h2>
   </div>
 </template>
