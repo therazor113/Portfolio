3cd81f949c99625e5d77ef35f0421f2c3b255adb
Razor <therazor113@gmail.com>
2022-07-31 15:34:33 -0400

    Working on contact info

--- src/components/contact/ContactView.vue
+++ src/components/contact/ContactView.vue
@@ -1,65 +1,89 @@
-<script>
-export default {
-  data () {
-    return {
-      nameText: '',
-      emailText: '',
-      messageText: ''
-    }
-  },
-  methods: {
-    onSubmit () {
-      console.log(`Name: ${this.nameText} Email: ${this.emailText} Message: ${this.messageText}`)
-    }
-  }
-}
-</script>
-
 <template>
   <h1 class="title">
     Contact
   </h1>
   <div class="contactContainer">
-    <div>
-      <h2>Send an email:</h2>
-      <form
-        name="EmailForm"
-        @submit.prevent="onSubmit"
-      >
-        <div class="formHeader">
-          <input
-            v-model="nameText"
-            required
-            class="name"
-            type="text"
-            placeholder="Enter Name"
-          >
-          <input
-            v-model="emailText"
-            required
-            class="email"
-            type="text"
-            placeholder="Enter Email"
-          >
-        </div>
-        <textarea
-          v-model="messageText"
-          required
-          class="message"
-          placeholder="Enter Message"
-          cols="30"
-          rows="10"
-        />
-        <input
-          class="submitButton"
-          type="submit"
-        >
-      </form>
-    </div>
     <div class="info">
-      <h2>LinkedIn:</h2>
-      <h2>Email:</h2>
-      <h2>GitHub:</h2>
+      <a
+        title="Linkedin"
+        href="https://www.linkedin.com/in/zachary-burtcher-473634209/"
+      >
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            fill="#444444"
+            viewBox="0 0 16 16"
+          >
+            <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+          </svg>
+          LinkedIn
+        </button>
+      </a>
+      <a
+        title="Email"
+        href="mailto:zachary.a.burtcher@gmail.com"
+      >
+        <button>
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#444444"
+              viewBox="0 0 512 512"
+              height="25"
+              width="25"
+            >
+              <path d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z" />
+            </svg>
+          </span>
+          Email
+        </button>
+      </a>
+      <a
+        title="Github"
+        href="https://github.com/therazor113"
+      >
+        <button>
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              fill="#444444"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+            </svg>
+          </span>
+          Github
+        </button>
+      </a>
+    </div>
+    <div class="resumeDiv">
+      <a
+        title="Resume"
+        href="/downloads/Resume.pdf"
+        rel="nofollow"
+        download
+      >
+        <button>
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              fill="#444444"
+              viewBox="0 0 384 512"
+            >
+              <path
+                d="M181.9 256.1c-5-16-4.9-46.9-2-46.9 8.4 0 7.6 36.9 2 46.9zm-1.7 47.2c-7.7 20.2-17.3 43.3-28.4 62.7 18.3-7 39-17.2 62.9-21.9-12.7-9.6-24.9-23.4-34.5-40.8zM86.1 428.1c0 .8 13.2-5.4 34.9-40.2-6.7 6.3-29.1 24.5-34.9 40.2zM248 160h136v328c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V24C0 10.7 10.7 0 24 0h200v136c0 13.2 10.8 24 24 24zm-8 171.8c-20-12.2-33.3-29-42.7-53.8 4.5-18.5 11.6-46.6 6.2-64.2-4.7-29.4-42.4-26.5-47.8-6.8-5 18.3-.4 44.1 8.1 77-11.6 27.6-28.7 64.6-40.8 85.8-.1 0-.1.1-.2.1-27.1 13.9-73.6 44.5-54.5 68 5.6 6.9 16 10 21.5 10 17.9 0 35.7-18 61.1-61.8 25.8-8.5 54.1-19.1 79-23.2 21.7 11.8 47.1 19.5 64 19.5 29.2 0 31.2-32 19.7-43.4-13.9-13.6-54.3-9.7-73.6-7.2zM377 105L279 7c-4.5-4.5-10.6-7-17-7h-6v128h128v-6.1c0-6.3-2.5-12.4-7-16.9zm-74.1 255.3c4.1-2.7-2.5-11.9-42.8-9 37.1 15.8 42.8 9 42.8 9z"
+              />
+            </svg>
+          </span>
+          Resume
+        </button>
+      </a>
     </div>
   </div>
 </template>
@@ -73,58 +97,44 @@ export default {
   }
   .contactContainer {
     display: flex;
-    justify-content: space-between;
+    flex-direction: column;
+    align-content: center;
     flex-wrap: wrap;
     margin: 2rem;
     width: 95%;
     height: 400px;
-    input {
-      padding: 5px;
-      background: black;
+    button {
+      transition: background-color 0.2s;
       border: none;
-      border-radius: 5px;
+      background-color: #181818;
       color: #eee;
-    }
-    form {
-      width: 415px;
-      display: flex;
-      flex-direction: column;
-      gap: 0.7rem;
+      line-height: 35px;
+      padding-left: 8px;
+      padding-right: 8px;
+      cursor: pointer;
+      font-size: 20px;
+      border-radius: 5px;
+      svg {
+        top: 5px;
+        transition: fill 0.2s;
+      }
+      &:hover {
+        background-color: rgba(80, 80, 80, 0.1);
+        svg {
+          fill: #3caa7d;
+        }
+      }
     }
   }
   .info {
     display: flex;
-    flex-direction: column;
-    margin-left: 3rem;
-    margin-right: 3rem;
+    align-content: center;
+    gap: 1rem;
+    right: 7px;
   }
-  .formHeader {
+  .resumeDiv {
+    margin-top: 1rem;
     display: flex;
-    justify-content: space-between;
-    input {
-      width: 200px;
-      height: 35px;
-    }
-  }
-  .message {
-    padding: 5px;
-    background: black;
-    resize: none;
-    width: 415px;
-    height: 275px;
-    border: none;
-    border-radius: 5px;
-    color: #eee;
-  }
-  .submitButton {
-    transition: background-color 0.3s;
-    width: 75px;
-    height: 30px;
-    margin: 0 auto;
-    &:hover {
-      transition: background-color 0.3s;
-      background-color: rgb(40, 40, 40);
-      cursor: pointer;
-    }
+    justify-content: center;
   }
 </style>
