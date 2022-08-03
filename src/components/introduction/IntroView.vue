b7b4d73ce8c6bb3c6ac1d86389db07bdfcbc146e
Razor <therazor113@gmail.com>
2022-08-02 21:58:34 -0400

    Moved download to public

--- src/components/introduction/IntroView.vue
+++ src/components/introduction/IntroView.vue
@@ -85,7 +85,7 @@ export default {
       </a>
       <a
         title="Resume"
-        href="/downloads/Resume.pdf"
+        href="/public/downloads/Resume.pdf"
         rel="nofollow"
         download
       >
