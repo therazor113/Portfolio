373b986daabf1526bbed1713420f8eae799ee860
Razor <therazor113@gmail.com>
2022-08-02 20:44:33 -0400

    Removed games

--- src/components/projects/ProjectView.vue
+++ src/components/projects/ProjectView.vue
@@ -1,7 +1,5 @@
 <script setup>
 import ForkliftProject from './items/ForkliftProject.vue'
-import TicTacToe from './items/games/TicTacToe.vue'
-import SnakeGame from './items/games/SnakeGame.vue'
 </script>
 
 <template>
@@ -12,20 +10,13 @@ import SnakeGame from './items/games/SnakeGame.vue'
       </h1>
       <ForkliftProject />
     </div>
-    <div>
-      <div>Lorem Ipsum</div>
-    </div>
-    <div class="games">
-      <TicTacToe />
-      <SnakeGame />
-    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
   .ProjectsContainer {
     width: 100%;
-    height: 1500px;
+    height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
@@ -35,9 +26,5 @@ import SnakeGame from './items/games/SnakeGame.vue'
       text-align: center;
       margin-bottom: 1rem;
     }
-  }
-  .games {
-    display: flex;
-    justify-content: space-around;
   }
 </style>
