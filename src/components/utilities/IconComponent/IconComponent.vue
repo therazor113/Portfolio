f0dd4a9c97eb1ef2062d86cbb3a05a0e99f6c535
Razor <therazor113@gmail.com>
2022-08-26 13:15:18 -0400

    Created iconComponent for creating icons

---
+++ src/components/utilities/IconComponent/IconComponent.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  props: ['title', 'size', 'href']
+}
+</script>
+
+<template>
+  <a
+    target="_blank"
+    :href="href"
+  >
+    <img
+      :class="title"
+      :title="title"
+      :alt="`${title} logo`"
+      :src="`./src/assets/icons/${title}-icon.svg`"
+      :width="size"
+      :height="size"
+    >
+    <slot />
+  </a>
+</template>
+
+<style lang="scss" scoped>
+  @import './styles.module.scss';
+</style>
