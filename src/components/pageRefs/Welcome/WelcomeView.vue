75211326b5826f86d0071f200096452263f8d7c8
Razor <therazor113@gmail.com>
2022-09-06 14:42:26 -0400

    Created seperate folder for text script

--- src/components/pageRefs/Welcome/WelcomeView.vue
+++ src/components/pageRefs/Welcome/WelcomeView.vue
@@ -1,46 +1,4 @@
-<script>
-export default {
-  data () {
-    return {
-      sentence: '',
-      sentenceArr: ['Full Stack Developer', 'JavaScript Tinkerer', 'Gaming Enthusiast', 'Space Enjoyer'],
-      tempArr: [],
-      count: 0
-    }
-  },
-  created () {
-    this.start()
-  },
-  methods: {
-    async start () {
-      for (let i = 0; i < this.sentenceArr.length; i++) {
-        this.tempArr = this.sentenceArr[this.count].split('')
-        await this.changeText(true, 125)
-        await this.changeText(false, 100)
-        this.count++
-      }
-      this.count = 0
-      this.start()
-    },
-    changeText (e, n) {
-      return new Promise(resolve => {
-        for (let i = 0; i < this.tempArr.length; i++) {
-          setTimeout(() => {
-            if (e) {
-              this.sentence += this.tempArr[i]
-            } else if (!e) {
-              this.sentence = this.sentence.slice(0, -1)
-            }
-            if (i === this.tempArr.length - 1) {
-              setTimeout(resolve, 1000)
-            }
-          }, i * n)
-        }
-      })
-    }
-  }
-}
-</script>
+<script src="./WelcomeText"></script>
 
 <template>
   <div class="introContainer">
