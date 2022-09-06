e655710a31e7f0955d304812972f59d47a8a1940
Razor <therazor113@gmail.com>
2022-09-06 14:41:08 -0400

    Created NavMenu for mobile users

---
+++ src/components/core/Navbar/NavMenu/NavMenu.vue
@@ -0,0 +1,34 @@
+<script>
+import NavLinks from '../NavLinks/NavLinks.vue'
+export default {
+  components: { NavLinks },
+  data () {
+    return {
+      showMenu: false
+    }
+  }
+}
+</script>
+
+<template>
+  <div
+    class="navMenuButton"
+    tabindex="0"
+    @click="showMenu = !showMenu"
+    @blur="showMenu = false"
+  >
+    <span :style="showMenu && 'transform: scale(1.05); background-color: rgba(235, 235, 235, 0.64);'" />
+    <span :style="showMenu && 'transform: scale(1.05); background-color: rgba(235, 235, 235, 0.64);'" />
+    <span :style="showMenu && 'transform: scale(1.05); background-color: rgba(235, 235, 235, 0.64);'" />
+  </div>
+  <div
+    :style="showMenu ? 'transform: scaleY(1); opacity: 1;' : 'transform: scaleY(0); opacity: 0;'"
+    class="navMenu"
+  >
+    <NavLinks />
+  </div>
+</template>
+
+<style lang='scss' scoped>
+  @import './styles.module.scss';
+</style>
