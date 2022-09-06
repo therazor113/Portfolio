5efe87ba7650970197b2dc4ae9f3474409e04367
Razor <therazor113@gmail.com>
2022-09-06 14:40:49 -0400

    Created seperate file for navLink

---
+++ src/components/core/Navbar/NavLinks/NavLinks.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="navLinks">
+    <ScrollLink href="#home">
+      <h2>Home</h2>
+    </ScrollLink>
+    <ScrollLink href="#about">
+      <h2>About</h2>
+    </ScrollLink>
+    <ScrollLink href="#projects">
+      <h2>Projects</h2>
+    </ScrollLink>
+    <ScrollLink href="#contact">
+      <h2 class="contact">
+        Contact
+      </h2>
+    </ScrollLink>
+  </div>
+</template>
